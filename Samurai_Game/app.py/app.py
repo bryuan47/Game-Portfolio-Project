f97fe6d150e97d4ea973cfdb6dcf716465391527
Bryuan <bryuan47@gmail.com>
2026-01-06 12:38:47 -0500

    boywalking

--- Samurai_Game/app.py/app.py
+++ Samurai_Game/app.py/app.py
@@ -4,12 +4,16 @@ pygame.init()
 #making the main loop
 s_width = 600
 s_height = 400
-x= 0
-y = 0
+x= 50
+y = 345
 vel = 5
+left = False
+right = False
+walkCount = 0
 
 screen = pygame.display.set_mode((s_width, s_height))
 clock = pygame.time.Clock
+walkRight = [pygame.image.load(f"Idle ({i}).png") for i in range(1,15)]
 
 
 
@@ -19,7 +23,10 @@ bg = pygame.transform.scale(bg,(s_width, s_height))
 boy = pygame.image.load("Idle (1).png").convert_alpha()
 boy = pygame.transform.scale(boy,(100,100))
 
-
+def Gameloop():
+    global walkCount
+    
+    
 
 
 
@@ -35,6 +42,8 @@ while done:
     screen.blit(boy,(x,y))
     if keys[pygame.K_LEFT]:
         x = x - vel
+        left = True
+        right = False
     if keys[pygame.K_RIGHT]:
         x = x + vel
     
