439408bb8d3699d66223a283d65a734a401d4483
Bryuan Mathis <bryuan47@gmail.com>
2026-01-06 21:33:19 -0500

    movement_working

--- Samurai_Game/app.py/app.py
+++ Samurai_Game/app.py/app.py
@@ -11,28 +11,49 @@ left = False
 right = False
 walkCount = 0
 
+neg = 1
+
 screen = pygame.display.set_mode((s_width, s_height))
 clock = pygame.time.Clock()
-walkRight = [pygame.image.load(f"Run ({i}).png") for i in range(1,15)]
+walkRight = [pygame.transform.scale(pygame.image.load(f"Game-Portfolio-Project\Run ({i}).png").convert_alpha(),(100,100)) for i in range(1,9)]
+walkLeft = [pygame.transform.flip(pygame.transform.scale(pygame.image.load(f"Game-Portfolio-Project\Run ({i}).png").convert_alpha(),(100,100)), True, False) for i in range(1,9)]
 
 
 
 pygame.display.set_caption("Samurai Showdown")
 bg = pygame.image.load("orig.png")
 bg = pygame.transform.scale(bg,(s_width, s_height))
-boy = pygame.image.load("Idle (1).png").convert_alpha()
+boy = pygame.image.load("Game-Portfolio-Project\Idle (1).png").convert_alpha()
 boy = pygame.transform.scale(boy,(100,100))
 
 def Gameloop():
+    clock.tick(60)
     global walkCount
-    if walkCount +1 >= 15:
+    if walkCount +1 >= 9:
         walkCount = 0
     
+
     if right == True:
-        screen.blit(walkRight[walkCount],(x,y))
+        screen.blit(walkRight[walkCount //2],(x,y))
         walkCount = walkCount +1 
+       
+    elif left == True:
+        screen.blit(walkLeft[walkCount //2],(x,y))
+        walkCount = walkCount +1 
+       
+    else: 
+        walkCount = 0
+        screen.blit(boy,(x,y))
+    
 
-
+    
+    pygame.display.flip()
+    
+    
+        
+        
+is_jump = False
+jump_count = 10
 
 
 #Game Loop 
@@ -43,26 +64,49 @@ while done:
             done = False
     keys= pygame.key.get_pressed()
     screen.blit(bg,(0,0))
-    screen.blit(boy,(x,y))
-    if keys[pygame.K_LEFT]:
+   
+    if keys[pygame.K_LEFT] and x > 0:
         x = x - vel
         left = True
         right = False
-    if keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_RIGHT] and x < s_width -100:
         x = x + vel
         left = False 
         right = True
-    else: 
+    else:
         right = False
         left = False
-        walkCount = 0 
+    
+    
+    
+    
+ 
+    if keys[pygame.K_SPACE] and not is_jump and y < s_height:
+        is_jump = True
+        
+    
+    
+    if is_jump:
+            if jump_count >= -10:
+                neg = 1
+                if jump_count <0:
+                    neg = -1
+                y = y - (jump_count ** 2) * neg * 0.5 # reduce jump height by dividing by 2
+                jump_count= jump_count -1   
+            else: 
+                jump_count = 10
+                is_jump = False
+        
+
+   
+    
     
     
     
 
     Gameloop()
 
-    pygame.display.flip()
+  
 
 
 
